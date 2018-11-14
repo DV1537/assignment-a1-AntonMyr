710d1673a8f298d6de57f7385bf3f1edb8b34f7a
antonmyr <15anmy@stockholmscience.se>
2018-11-14 18:31:52 +0100

    Added some istream functions to make the code work

--- main.cpp
+++ main.cpp
@@ -13,26 +13,47 @@ int main(int argc, const char * argv[])
 
 
   /**
-    * - should store the all the numbers in an dynamic array
+    * - should store all the numbers in an dynamic array
     * - should calculate the average
     * - Should display all the values above the average
     */
   int a = 0;
   int sum = 0;
+  int charCounter = 0;
 
   std::ifstream myReadFile;
 
   myReadFile.open(argv[1]);
 
   while (myReadFile >> a) {
-      sum += a;
+    charCounter += 1;
   }
 
+  /**
+    * Clears myReadFile from eof bit
+    */
+  myReadFile.clear();
+  myReadFile.seekg(0, std::ios::beg);
+  a = 0;
+
+  int* numbersArray = new int[charCounter];
+  for(int i = 0; i < charCounter; i++) {
+    myReadFile >> a;
+    numbersArray[i] = a;
+    sum += a;
+  }
+
+  sum /= charCounter;
+
+  for(int i = 0; i < charCounter; i++) {
+    if(numbersArray[i] > sum) {
+      std::cout << numbersArray[i] << std::endl;
+    }
+  }
+
+  delete [] numbersArray;
+
   myReadFile.close();
-
-
-
-  std::cout << sum << "\n";
 
   return 0;
 }
